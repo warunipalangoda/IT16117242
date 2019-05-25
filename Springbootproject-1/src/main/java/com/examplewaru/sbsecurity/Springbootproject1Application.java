a588672b1455b9938e17c448db442f843ac3bc70
warunipalangoda <47710719+warunipalangoda@users.noreply.github.com>
2019-05-25 22:51:55 +0530

    Add files via upload

---
+++ Springbootproject-1/src/main/java/com/examplewaru/sbsecurity/Springbootproject1Application.java
@@ -0,0 +1,13 @@
+package com.examplewaru.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootproject1Application.class, args);
+	}
+
+}
---
+++ Springbootproject-1/src/main/java/com/examplewaru/sbsecurity/service/UserDetailsServiceImpl.java
@@ -0,0 +1,56 @@
+package com.examplewaru.sbsecurity.service;
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+import com.examplewaru.sbsecurity.dao.AppUserDao;
+import com.examplewaru.sbsecurity.entity.AppUser;
+import com.examplewaru.sbsecurity.dao.AppRoleDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+ 
+    @Autowired
+    private AppUserDao appUserDAO;
+ 
+    @Autowired
+    private AppRoleDao appRoleDAO;
+ 
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        AppUser appUser = this.appUserDAO.findUserAccount(userName);
+ 
+        if (appUser == null) {
+            System.out.println("User not found! " + userName);
+            throw new UsernameNotFoundException("User " + userName + " was not found in the database");
+        }
+ 
+        System.out.println("Found User: " + appUser);
+ 
+        // [ROLE_USER, ROLE_ADMIN,..]
+        List<String> roleNames = this.appRoleDAO.getRoleNames(appUser.getUserId());
+ 
+        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+        if (roleNames != null) {
+            for (String role : roleNames) {
+                // ROLE_USER, ROLE_ADMIN,..
+                GrantedAuthority authority = new SimpleGrantedAuthority(role);
+                grantList.add(authority);
+            }
+        }
+ 
+        UserDetails userDetails = (UserDetails) new User(appUser.getUserName(), //
+                appUser.getEncrytedPassword(), grantList);
+ 
+        return userDetails;
+    }
+ 
+}
